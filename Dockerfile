0cd82085973bdf29152de54134061d882256a0e4
Sanchal Khedkar <khedkarsanchal86@gmail.com>
2025-11-04 15:04:31 +0530

    all changes

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+
+#Frontend Dockerfile
+
+# Stage 1: Build Angular App
+FROM node:18-alpine AS builder 
+
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm install -g @angular/cli && \
+    ng build --configuration production
+
+# Stage 2: Serve with Nginx
+FROM nginx:alpine
+COPY --from=builder /app/dist/ /usr/share/nginx/html
+
+
+EXPOSE 8081
+CMD ["nginx", "-g", "daemon off;"]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,35 @@
 import { JsonPipe } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
-  styleUrl: './app.css',
-  providers: [HttpClientModule],
+  styleUrls: ['./app.css']
 })
-export class App {
+export class App implements OnInit {
   protected title = 'TestEnv';
-  private testURL =
-    import.meta.env['NG_APP_URL'] || "'https://task.thingsrms.com/v1";
   data: any = null;
+  config: any;
+  apiUrl: string = '';
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    console.log(this.testURL);
-    this.http.get<any>(this.testURL).subscribe((data) => {
-      this.data = data;
+    // Step 1: Load config.json dynamically
+    this.http.get('/assets/config.json').subscribe((config) => {
+      this.config = config;
+      this.apiUrl = this.config['NG_APP_URL'] || 'https://task.thingsrms.com/v1';
+      console.log('Loaded API URL:', this.apiUrl);
+
+      // Step 2: Call API using the runtime URL
+      this.http.get(this.apiUrl).subscribe((data) => {
+        this.data = data;
+        console.log('Response:', this.data);
+      });
     });
   }
 }
